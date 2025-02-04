45e9c87cc393eb516776bea45cc756578d598f37
turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-02-04 15:20:40 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const submitBtn = document.getElementById("submit-btn");
+const fName = document.getElementById("first-name");
+const lName = document.getElementById("last-name");
+const email = document.getElementById('email');
+const subject = document.getElementById('subject');
+const message = document.getElementById('message')
+const form = document.getElementById("form");
+
+window.addEventListener("load", () => {
+    form.reset();
+})
