97ef33c756746d9206b14949a2a9b2902d13d4a8
turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-02-10 14:27:33 -0600

    added form fields

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const fName = document.getElementById("first-name");
 const lName = document.getElementById("last-name");
 const email = document.getElementById('email');
 const subject = document.getElementById('subject');
-const message = document.getElementById('message')
+const message = document.getElementById('message');
+const record = document.getElementById('recording');
+const mix = document.getElementById('mixing');
+const master = document.getElementById('mastering');
+const genre = document.getElementById('genre');
 const form = document.getElementById("form");
 
 window.addEventListener("load", () => {
@@ -12,4 +16,9 @@ window.addEventListener("load", () => {
     email.value = "";
     subject.value = "";
     message.value = "";
+    record.checked = false;
+    mix.checked = false;
+    master.checked = false;
+    genre.selectedIndex = 0;
+
 })
